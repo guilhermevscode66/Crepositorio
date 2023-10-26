af30967f3dac620309626c5153fdaa7b1e545a8e
guilhermevscode66 <148369746+guilhermevscode66@users.noreply.github.com>
2023-10-26 19:09:01 -0300

    Add files via upload
    variables declaration and basic functions...

---
+++ electronic urn simulator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include"fmod.h"
+#include<stdlib.h>
+#define int function function;
+function recebecandidato(float cand_a, float cand_b){
+    if(cand_a>=50/100+1){
+
+    }
+}    
